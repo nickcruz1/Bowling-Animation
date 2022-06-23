9ae2363c07849b0fc8b619f70edcd73ec4a54887
Nick Cruz <nickcruz920@gmail.com>
2022-06-23 06:27:55 -0400

    Added function for bowling ball pins animation

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function startRoll() {
   ball.style.transition = "3s";
   ball.style.transform = "translateY(-200px)";
 }
+
+// Function for Pins Animation 
+function movePins() {
+  
+    let pin1 = document.getElementById("pin1");
+    pin1.style.transform = "translate(-10px)";
+    pin1.style.transition = "1s";
+    pin1.style.transform = "rotate(360deg)";
+     
+     let pin2 = document.getElementById("pin2");
+     pin2.style.transform = "translate(-10px)";
+     pin2.style.transition = "1s";
+     pin2.style.transform = "rotate(360deg)";
+     
+   }
